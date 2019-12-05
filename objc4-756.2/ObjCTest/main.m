8d9f637ce529ba8d96e61d8aad99a8bf3a51e214
Jesse Xu <jessexu.mail@qq.com>
2019-12-05 18:39:16 +0800

    build code

---
+++ objc4-756.2/ObjCTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ObjCTest
+//
+//  Created by jesse.xu on 2019/12/5.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
